ad256f964a8a35e3c0168a1903f82f85fcbfd5a9
max <29782397+tekoh@users.noreply.github.com>
2023-06-06 18:00:30 +0100

    fix: i think

--- functions/fish_right_prompt.fish
+++ functions/fish_right_prompt.fish
@@ -1,7 +1,7 @@
 function fish_right_prompt
   set -l cyan (set_color -o cyan)
 
-  if "$theme_show_right_pwd" = 'no'
+  if set -q $theme_show_right_pwd
     echo ""
   else
     echo -n -s $cyan (prompt_pwd)
