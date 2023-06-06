82315e758f953a14b62c10cd65e015ccdaadd413
max <29782397+tekoh@users.noreply.github.com>
2023-06-06 18:06:39 +0100

    fix: remove bold?

--- functions/fish_right_prompt.fish
+++ functions/fish_right_prompt.fish
@@ -1,5 +1,6 @@
 function fish_right_prompt
   set -l cyan (set_color -o cyan)
+  set -l normal (set_color normal)
 
   if set -q $theme_show_right_pwd
     echo ""
@@ -7,7 +8,7 @@ function fish_right_prompt
     if test (prompt_pwd) = "~"
       echo ""
     else
-      echo -n -s $cyan (prompt_pwd)
+      echo -n -s $cyan (prompt_pwd) $normal
     end
   end
 end
